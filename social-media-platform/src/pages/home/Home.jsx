50127b46ac5551463d99563044bed4254a94d5a0
jiatongcherry <jiatongcherry@gmail.com>
2025-04-04 12:05:18 +0800

    home upgrade

--- social-media-platform/src/pages/home/Home.jsx
+++ social-media-platform/src/pages/home/Home.jsx
@@ -1,17 +1,24 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import Topbar from '../../components/topbar/Topbar';
 import Sidebar from '../../components/sidebar/Sidebar';
 import Rightbar from '../../components/rightbar/Rightbar';
 import Feed from '../../components/feed/Feed';
 import './home.css'
+import { UserContext } from '../../UserContext';
 
 const Home = () => {
+  const { currentUser } = useContext(UserContext);
+  // const allUsernames = [
+  //   currentUser.username,
+  //   ...(currentUser.followings?.map(user => user.username) || [])
+  // ];
+
   return (
     <>
       <Topbar />
       <div className="homeContainer">
         <Sidebar />
-        <Feed />
+        <Feed username = {currentUser.username}/>
         <Rightbar />
       </div>
     </>
